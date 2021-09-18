26df57a56d1da3df17d320aed0717e59b0ee2e0c
youssefyy <ysf.ysr@gmail.com>
2021-09-18 12:08:39 +0200

    feat(index.js/ helper.js): sort function

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import { pipe, always, applySpec } from 'ramda';
-
-const getValue = (o, sortKey) => sortKey ? o[sortKey] : o
+import { mergeSort } from './helper.js';
 
 const sort = (list, sortKey) => {
   // SHOULD IMPLEMENT
-  return list;
+  return mergeSort(list, sortKey);
 };
 
 const findIndex = (list, sortKey) => value => {
@@ -43,3 +42,32 @@ export const List = ({ sortKey, initial, initialOrder}) => {
     })
   }
 }
+
+// function merge(left, right, sortKey) {
+//     let arr = []
+//     // Break out of loop if any one of the array gets empty
+//     while (left.length && right.length) {
+//         // Pick the smaller among the smallest element of left and right sub arrays
+//         // if (left[0][sortKey] < right[0][sortKey]) {
+//         if (getValue(left[0], sortKey) < getValue(right[0], sortKey)) {
+//             arr.push(left.shift());
+//         } else {
+//             arr.push(right.shift());
+//         }
+//     }
+//     // Concatenating the leftover elements
+//     // (in case we didn't go through the entire left or right array)
+//     return [ ...arr, ...left, ...right ]
+//   }
+  
+//   function mergeSort(array, sortKey) {
+//     const half = array.length / 2;
+    
+//     // Base case or terminating case
+//     if(array.length < 2){
+//       return array;
+//     }
+    
+//     const left = array.splice(0, half);
+//     return merge(mergeSort(left, sortKey), mergeSort(array, sortKey), sortKey);
+//   }
